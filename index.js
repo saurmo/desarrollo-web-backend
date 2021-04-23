cf7aa9ade45fbe5a427dfb84df66943ac428bfc6
saurmo <santiago@creativosdigitales.co>
2021-04-23 07:08:47 -0500

    - Instalación de cors

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
  */
 
 const express = require("express");
+const cors = require("cors");
 const router = require("./app/routers/index");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/", router);
 
