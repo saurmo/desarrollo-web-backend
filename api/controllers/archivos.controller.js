80c5011c5d0ef03db3f3dd1dbf57f8cafed9a2f3
saurmo <saurmo0108@gmail.com>
2021-10-20 07:12:09 -0500

    - crud archivos

--- api/controllers/archivos.controller.js
+++ api/controllers/archivos.controller.js
@@ -15,5 +15,18 @@ const guardarArchivoDb = async (archivo) => {
 
 }
 
-module.exports = { guardarArchivoDb }
+
+const consultarArchivoDb = async (id_padre, tipo_padre) => {
+    const sql = 'SELECT * FROM archivos WHERE id_padre=$1 and tipo_padre=$2'
+    const datos = [id_padre, tipo_padre]
+    return await _pg.ejecutarQuery(sql, datos)
+}
+
+const eliminarArchivoDb = async (id) => {
+    const sql = 'DELETE FROM archivos WHERE id=$1'
+    const datos = [id]
+    return await _pg.ejecutarQuery(sql, datos)
+}
+
+module.exports = { guardarArchivoDb, consultarArchivoDb, eliminarArchivoDb }
 
