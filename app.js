73d2d7a62a354cba8679c2b58db14bef2f5113d3
dwn84 <50523550+dwn84@users.noreply.github.com>
2022-09-15 19:42:24 -0500

    Instalacion cors

--- app.js
+++ app.js
@@ -2,10 +2,16 @@
 // Importar el framework
 const express = require('express')
 
+// Importar lib de cors 
+const cors = require('cors');
+
 // Crear una instancia 
 const app = express()
 const routerProducts = require('./src/routers/productos/productos.router');
 const routerBuys = require("./src/routers/compras/compras.router")
+
+// USAR CORS 
+app.use(cors())
 
 // DEFINICION DE ENDPOINSTS
 app.use(routerProducts)
