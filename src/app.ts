1c27c4801cc2a9fc80f29bbe2c6561ad88a533bc
B12_307_22\s307e22 <profesor1@udem.edu.co>
2026-09-22 19:22:21 -0500

    feat: cors

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.get('/', (req: Request, res: Response) => {
 // Middleware es global: express.json() permite capturar el json de los bodies
 app.use(express.json());
 app.use(cors({
-  origin:["http://localhost:3000", "https://listings-app-nine.vercel.app/"]
+  origin:["http://localhost:3000", "https://listings-app-nine.vercel.app"]
 }))
 
 
